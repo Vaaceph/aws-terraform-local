4a1da13ce09100f41039f7b7fdfd3099379a4da3
Muhammad Vaaceph Khan <vaaceph.khan@lampenwelt.de>
2021-07-16 08:56:28 +0200

    Add backend

--- main.tf
+++ main.tf
@@ -43,12 +43,12 @@ resource "aws_instance" "app_server_2" {
 
 ## Specifies the S3 Bucket and DynamoDB table used for the durable backend and state locking
 
-# terraform {
-#     backend "s3" {
-#       encrypt = true
-#       bucket = "test-repository"
-#       dynamodb_table = "terraform-state-lock-dynamo-2"
-#       key = "terraform.tfstate"
-#       region = "eu-central-1"
-#   }
-# }
+terraform {
+    backend "s3" {
+      encrypt = true
+      bucket = "test-repository"
+      dynamodb_table = "terraform-state-lock-dynamo-2"
+      key = "terraform.tfstate"
+      region = "eu-central-1"
+  }
+}
